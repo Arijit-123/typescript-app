328b5eb9679c044ddc1bf7ee932bf2ea30fd35c7
Arijit-123 <1603074@kiit.ac.in>
2023-09-11 09:09:59 +0530

    first commit

---
+++ newtypescriptapp/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './App.css';
+import Inputfeilds from './Components/Inputfeilds';
+
+
+
+const App:React.FC = () => {
+  return (
+    <div className="App">
+      <span className='heading'>Taskify</span>
+      <Inputfeilds/>
+
+    </div>
+  )
+}
+
+export default App;
+
+
+  
+  
+
+  
+  
+   
---
+++ newtypescriptapp/src/Components/Inputfeilds.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Inputfeilds = () => {
+  return (
+    <div>
+      input feild
+      <form className='input'>
+        <input  placeholder='enter functions'></input>
+        <button className='input_submit'>GO</button>
+      </form>
+    </div>
+  )
+}
+
+export default Inputfeilds
